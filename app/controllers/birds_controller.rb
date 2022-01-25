e749e9a1d7ad258ac25785b8518c938f6ef72ef4
JubalThang <jubalthang@gmail.com>
2022-01-25 12:05:10 -0600

    custom render is added

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -2,14 +2,15 @@ class BirdsController < ApplicationController
 
     # GET /birds
     def index 
-        render json:Bird.all 
+        birds = Bird.all 
+        render json: birds, except: [:created_at, :updated_at]
     end
 
     # GET /birds/:id
     def show 
         bird = Bird.find_by(id: params[:id])
         if bird 
-            render json:bird, status: :ok
+            render json: bird, only: [:name], status: :ok
         else 
             render json: {error: "Bird not found!"}, status: :not_found
         end
